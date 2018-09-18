66e8ef14c02445570aab4469604d20839dc0f66b
JD Martin <jd.martin@teli.net>
2018-09-18 15:00:34 -0600

    update again.

--- teli_crm/__manifest__.py
+++ teli_crm/__manifest__.py
@@ -27,7 +27,7 @@
 
     # always loaded
     'data': [
-        'security/ir.model.access.csv',
+        # 'security/ir.model.access.csv',
         'views/views.xml',
         'views/templates.xml',
     ],
