7ab2c9b402ae7ee6a3c17fe84bb177b4baf32d6e
JD Martin <jd.martin@teli.net>
2018-09-19 17:48:01 -0600

    updating the lead to opportunity view.

--- teli_crm/wizard/teli_lead_to_opportunity_partner.py
+++ teli_crm/wizard/teli_lead_to_opportunity_partner.py
@@ -4,7 +4,8 @@ import logging
 from odoo import api, fields, models
 
 _logger = logging.getLogger(__name__)
-_logger.setLevel('DEBUG')
+# uncomment for debugging
+# _logger.setLevel('DEBUG')
 
 class teli_lead2opportunity_partner(models.TransientModel):
     _inherit = 'crm.lead2opportunity.partner'
@@ -18,6 +19,6 @@ class teli_lead2opportunity_partner(models.TransientModel):
 
     under_contract = fields.Char(string='Are open and available to review and bring on new vendors?', help='Under Contract?', required=True)
 
-    valid_use_case = fields.Boolean(string='Valid Use Case and Overview of their business model', required=True)
+    valid_use_case = fields.Boolean(string='Valid Use Case and Overview of their business model?', required=True)
 
     share_rates = fields.Boolean(string='Willing to share target rates?', required=True)
